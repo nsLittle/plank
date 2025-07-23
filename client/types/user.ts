718e24fa164386a86edd851c1ef9977d0958f049
nsLittle <notsolittle88@gmail.com>
2025-07-23 17:42:17 -0400

    Remove client submodule reference

--- client/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface UserContextType {
-  email: string | null;
-  token: string | null;
-}
-
-export interface UserContextValue {
-  userContext: UserContextType;
-  setUserContext: React.Dispatch<React.SetStateAction<UserContextType>>;
-}
